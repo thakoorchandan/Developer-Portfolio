bd324dbe03d259aff09e8a8ff6eed2d00a286b63
Thakoor Chandan <tchandan@tekion.com>
2023-06-26 02:01:33 +0530

    Portfolio Ready

---
+++ README.md
@@ -0,0 +1,123 @@
+<h1 align="center">Welcome to Portfolio 👋</h1>
+<a href="https://github.com/thakoorchandan/Portfolio/blob/main/LICENSE"><img alt="GitHub license" src="https://img.shields.io/github/license/thakoorchandan/Portfolio"></a><a href="https://github.com/thakoorchandan/Portfolio/issues"><img alt="GitHub issues" src="https://img.shields.io/github/issues/thakoorchandan/Portfolio"></a><a href="https://github.com/thakoorchandan/Portfolio/network"><img alt="GitHub forks" src="https://img.shields.io/github/forks/thakoorchandan/Portfolio"></a> <a href="https://github.com/thakoorchandan/Portfolio/stargazers"><img alt="GitHub stars" src="https://img.shields.io/github/stars/thakoorchandan/Portfolio"></a>
+
+## Software Developer Portfolio Template built with Next.js bootstrap that helps you showcase your work and skills as a software developer.
+
+<p align="center">
+  <kbd>
+    <img src="https://github.com/thakoorchandan/Portfolio/blob/main/public/images/portfolio.png"></img>
+  </kbd>
+</p>
+
+### To view a live example, **[click here](https://thakoorchandan-portfolio.vercel.app/)**.
+
+Just Edit `portfolio.js` to get your personal portfolio. Feel free to use it as-is or customize it as much as you want.
+
+But if you want to **contribute** and make this much better for other developer have a look at [Issues](https://github.com/thakoorchandan/Portfolio/issues).
+
+If you created something awesome and want to contribute then feel free to open an [pull request](https://github.com/thakoorchandan/Portfolio/pulls).
+
+## Table of Contents
+
+- [Sections](#sections)
+- [Getting Started](#getting-started)
+- [How to Use](#how-to-use)
+- [Linking portfolio to Github](#linking-portfolio-to-github)
+- [Change and Customize](#change-and-customize-every-section-according-to-your-need)
+- [Technologies Used](#technologies-used)
+- [Illustrations](#illustrations)
+- [For the Future](#for-the-future)
+- [Contributors](#project-maintainers)
+
+## Sections
+
+✔️ Summary and About me\
+✔️ Skills\
+✔️ Education\
+✔️ Work Experience\
+✔️ Projects\
+✔️ Github Profile
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+You'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer.
+<br>
+**For Windows** Install Visual C++ Build Environment: [Visual Studio Build Tools](https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools)
+
+```
+node@v10.16.0 or higher
+npm@6.9.0 or higher
+git@2.17.1 or higher
+```
+
+## How To Use
+
+From your command line, clone and run developer-portolio:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/thakoorchandan/Portfolio.git
+
+# Go into the repository
+$ cd Portfolio
+
+# Install dependencies
+$ yarn
+
+#Start's development server
+$ yarn dev
+```
+
+## Linking Portfolio to Github
+
+```javascript
+  //  portfolio.js
+  githubUserName: 'YOUR GITHUB USERNAME HERE',
+```
+
+#### Using Emojis
+
+For adding emoji 😃 into the texts in `Portfolio.js`, use the `emoji()` function and pass the text you need as an argument. This would help in keeping emojis compatible across different browsers and platforms.
+
+## Technologies Used
+
+- [Next.js](https://nextjs.org/)
+- [axios](https://www.npmjs.com/package/axios)
+- [reactstrap](https://reactstrap.github.io/)
+- [react-reveal](https://www.react-reveal.com/)
+- [react-lottie](https://www.npmjs.com/package/react-lottie)
+- [react-easy-emoji](https://github.com/appfigures/react-easy-emoji)
+- [react-headroom](https://github.com/KyleAMathews/react-headroom)
+- [color-thief](https://github.com/lokesh/color-thief)
+
+## Illustrations
+
+- [Lottie File Source](https://lottiefiles.com)
+
+## For the Future
+
+If you can help us with these. Please don't hesitate to open a [pull request](https://github.com/thakoorchandan/Portfolio/pulls).
+
+- Enable Dark Mode
+
+- Add More Sections
+
+## Author
+
+👤 **Thakoor Chandan**
+
+- Twitter: [@thakoorchandan](https://twitter.com/thakoor_chandan)
+- Github: [@thakoorchandan](https://github.com/thakoorchandan)
+- LinkedIn: [@thakoorchandan](https://www.linkedin.com/in/thakoor-chandan-4a78b5106/)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+- Read cvc
+  validate regex
+  mx records
+  tegex before @
+  domain records
---
+++ portfolio.ts
@@ -0,0 +1,214 @@
+import emoji from 'react-easy-emoji';
+
+import hilton_image from './public/images/Hilton-Hotels.png';
+import strawberrynet from './public/images/Strawberrynet.png';
+
+import {
+  EducationType,
+  ExperienceType,
+  FeedbackType,
+  ProjectType,
+  SkillsSectionType,
+  SkillBarsType,
+  SEODataType,
+  SocialLinksType,
+  GreetingsType,
+} from './types/sections';
+
+export const greetings: GreetingsType = {
+  name: 'Thakoor Chandan',
+  title: 'Hi all, Thakoor Chandan',
+  description:
+    'Software Engineer who loves to transform ideas into reality using code and developing web applications using the latest front-end and UI/UX technologies and full-fledged APIs. Motivated designer and developer with experience creating custom websites with ReactJs, Node, Express, Mongo, and React JS.',
+  resumeLink:
+    'https://drive.google.com/file/d/1ksyzvyWheHgbZuw13_aTTGt6JaXALw9h/view?usp=sharing',
+};
+
+export const openSource = {
+  githubUserName: 'thakoorchandan',
+};
+
+export const contact = {};
+
+export const socialLinks: SocialLinksType = {
+  facebook: 'https://www.facebook.com/cherrychandantakur',
+  instagram: 'https://www.instagram.com/chandansingh__inc/?hl=en',
+  twitter: 'https://twitter.com/thakoor_chandan',
+  github: 'https://github.com/thakoorchandan',
+  linkedin: 'https://www.linkedin.com/in/thakoor-chandan-4a78b5106/',
+};
+
+export const skillsSection: SkillsSectionType = {
+  title: 'What I do',
+  subTitle: 'CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK',
+  data: [
+    {
+      title: 'Full Stack Development',
+      lottieAnimationFile: '/lottie/skills/fullstack.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji(
+          '⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js'
+        ),
+        emoji('⚡ Building responsive static websites using Next.js'),
+        emoji('⚡ Building RESTful APIs in Django & Django REST Framework'),
+      ],
+      softwareSkills: [
+        {
+          skillName: 'HTML-5',
+          fontAwesomeClassName: 'vscode-icons:file-type-html',
+        },
+        {
+          skillName: 'CSS-3',
+          fontAwesomeClassName: 'vscode-icons:file-type-css',
+        },
+        {
+          skillName: 'JavaScript',
+          fontAwesomeClassName: 'logos:javascript',
+        },
+        {
+          skillName: 'Reactjs',
+          fontAwesomeClassName: 'vscode-icons:file-type-reactjs',
+        },
+        {
+          skillName: 'Nextjs',
+          fontAwesomeClassName: 'vscode-icons:file-type-light-next',
+        },
+        {
+          skillName: 'Redux',
+          fontAwesomeClassName: 'logos:redux',
+        },
+        {
+          skillName: 'NPM',
+          fontAwesomeClassName: 'logos:npm-icon',
+        },
+        {
+          skillName: 'Yarn',
+          fontAwesomeClassName: 'logos:yarn',
+        },
+      ],
+    },
+  ],
+};
+
+export const SkillBars: SkillBarsType[] = [
+  {
+    Stack: 'Frontend/Design', //Insert stack or technology you have experience in
+    progressPercentage: '90', //Insert relative proficiency in percentage
+  },
+  {
+    Stack: 'Backend',
+    progressPercentage: '70',
+  },
+  {
+    Stack: 'Programming',
+    progressPercentage: '60',
+  },
+];
+
+export const educationInfo: EducationType[] = [
+  {
+    schoolName: 'MASAI SCHOOL',
+    subHeader: 'Full Stack Web Development',
+    duration: 'April 2021 - December 2021',
+    desc: 'Pursued Full-Stack Web Development Bootcamp.',
+    descBullets: [
+      'MERN Stack',
+      'Data Structures and Algorithms',
+      'Soft skills',
+      'Hackathons',
+    ],
+  },
+  {
+    schoolName: 'Vivekananda Institute of Tech and Sci',
+    subHeader: 'Computer Science and Engineering',
+    duration: 'August 2016 - December 2020',
+    desc: 'Recieved Young Scientist Award in my first year',
+    descBullets: [
+      'Operating Systems',
+      'Database Management System',
+      'Basics of Programming',
+      'Software Engineering Basics',
+    ],
+  },
+];
+
+export const experience: ExperienceType[] = [
+  {
+    role: 'Frontend Developer',
+    company: 'Tekion Corp',
+    companyLogo:
+      'https://media.licdn.com/dms/image/C560BAQGB0W6_ixPclA/company-logo_200_200/0/1547049504116?e=2147483647&v=beta&t=Y17rrlkwdW8CKCWbsuMn4c_Lx9t-gtQvwaUEPfOteT0',
+    date: 'Feb 2022 – Present',
+    desc: 'I worked as a frontend web developer for Vehicle Inventory Module on ARC and DRP projects',
+    descBullets: [
+      "Designed and implemented new features and functionality for web and mobile applications",
+"Collaborated with cross-functional teams to troubleshoot and resolve software issues",
+"Improved application performance by optimizing code and reducing load times",
+"Ensured software quality by conducting unit and integration tests",
+"Utilized Agile methodology to manage project timelines and deliverables",
+    ],
+  },
+];
+
+export const projects: ProjectType[] = [
+  {
+    name: 'developer-portfolio',
+    desc: 'Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.',
+    github: 'https://github.com/thakoorchandan/Portfolio',
+    link: 'https://thakoorchandan-portfolio.vercel.app/',
+  },
+  {
+    name: 'Strawberrynet.com',
+    desc: 'An E-Commerce website which speciaizes in selling beauty products all over the globe. The website provides 800 brands and 33,000 products of different types of makeup, skincare, body care, fragrances and home scents.',
+    stack: ['HTML, ', 'CSS, ', 'Express.js,  ', 'Node.js,  ', 'MongoDb'],
+    github: 'https://github.com/akhiladepu/re-clone-strawberrynet',
+    link: 'https://strawberrynet-clone-frontend.vercel.app/',
+  },
+  {
+    name: 'Hilton-Hotels',
+    desc: "Hilton Hotels & Resorts is Hilton's flagship brand and one of the largest hotel brands in the world. The brand is targeted at both business and leisure travelers with locations in major city centers around the world.",
+    stack: [
+      'React.js,  ',
+      'Material UI,  ',
+      'Express.js,  ',
+      'Node.js,  ',
+      'MongoDb',
+    ],
+    github: 'https://github.com/thakoorchandan/Hotels-by-Hilton',
+    link: 'https://hotels-by-hilton-44jhnnl0y-thakoorchandan.vercel.app/',
+  },
+];
+
+// option to hide or show the ContactUs component
+export const showContactUs: boolean = true;
+
+// See object prototype on /types/section.ts page
+export const seoData: SEODataType = {
+  title: 'Thakoor Chandan',
+  description: 'A passionate Full Stack Web Developer',
+  author: 'Thakoor Chandan',
+  url: 'https://thakoorchandan-portfolio.vercel.app/',
+  keywords: [
+    'Thakoor',
+    'Thakoor Chandan',
+    '@thakoorchandan',
+    'thakoorchandan',
+    'Portfolio',
+    'Thakoor Chandan Portfolio ',
+    'Thakoor Chandan Portfolio',
+    'web developer',
+    'full stack',
+    'full stack web developer',
+    'mobile app developer',
+    'android developer',
+    'django',
+    'flask',
+    'django rest framework',
+    'nodejs ',
+    'expressjs',
+    'reactjs ',
+    'contextapi',
+    'redux',
+    'flutter',
+  ],
+};
